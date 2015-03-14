2bdad2eb7686f51f95adb68cc536d664ada9f328
wangrun <wangrun20088002@126.com>
2015-03-14 16:08:02 +0800

    分类封装函数

--- page.func.php
+++ page.func.php
@@ -30,8 +30,10 @@ function showPage($page , $totalPage ,$where = null, $sep="&nbsp;" ) {
 	
 	$index = ($page == 1) ? "首页" : "<a href='{$url}?page=1{$where}'>首页</a>";
 	$last = ($page == $totalPage) ? "尾页" : "<a href='{$url}?page=$totalPage{$where}'>尾页</a>";
-	$prev = ($page == 1) ? "上一页" : "<a href='{$url}?page=({$page}-1){$where}'>上一页</a>";
-	$next = ($page == $totalPage) ? "下一页" : "<a href='{$url}?page=({$page}+1){$where}'>下一页</a>";
+	$prevPage=($page>=1)?$page-1:1;
+	$nextPage=($page==$totalPage)?$totalPage:$page+1;
+	$prev = ($page == 1) ? "上一页" : "<a href='{$url}?page=($prevPage){$where}'>上一页</a>";
+	$next = ($page == $totalPage) ? "下一页" : "<a href='{$url}?page=($nextPage){$where}'>下一页</a>";
 	$str = "总共{$totalPage}页，当前是{$page}页";
 	for($i = 1; $i <= $totalPage; $i ++) {
 		if ($page == $i) {
